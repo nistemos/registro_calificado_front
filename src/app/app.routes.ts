aff3742176e94fe8e2eeb7c2c3231ca3be4fc013
Nicolás Steeven Mosquera Gómez <34241505+nistemos@users.noreply.github.com>
2024-05-11 09:01:42 -0500

    vista login y no found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./login/routes'),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./page-not-found/page-not-found.component').then(
+        (m) => m.PageNotFoundComponent
+      ),
+  },
+  /*{
+    path: 'pruebaHolaMundo',
+    loadChildren: () =>
+      import('./hola-mundo/hola-mundo.component') as Promise<ItemsModule>,
+  },*/
+];
