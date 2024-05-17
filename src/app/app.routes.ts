b6418d032b24f812cc8284a2cd7477f1a7ff85eb
Andres Casallas <andrescasallas0520@hotmail.com>
2024-05-17 03:17:10 -0500

    dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import {HomeComponent} from './pages/home/home.component';
+import { EstudiantesComponent } from './pages/estudiantes/estudiantes.component';
+import { DocentesComponent } from './pages/docentes/docentes.component';
+
+
+
+
+
+export const routes: Routes = [
+    {
+        path:'home',
+        component:HomeComponent
+    },
+    {
+        path:'home2',
+        component:EstudiantesComponent
+    },
+    {
+        path:'home4',
+        component:DocentesComponent
+    },
+];
