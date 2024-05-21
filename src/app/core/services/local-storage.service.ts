5f48d78412637435e1287339ca49f88fd216d0ad
elvergatroncha <andres_penara@fet.edu.co>
2024-05-20 21:34:27 -0500

    su mama
    tambirn

--- src/app/core/services/local-storage.service.ts
+++ src/app/core/services/local-storage.service.ts
@@ -6,22 +6,21 @@ import { Injectable } from '@angular/core';
 export class LocalStorageService {
 
   constructor() { }
-  // Set a value in local storage
-  setItem(key: string, value: string): void {
-    localStorage.setItem(key, value);
-  }
 
-  // Get a value from local storage
-  getItem(key: string): string | null {
-    return localStorage.getItem(key);
-  }
 
   // Remove a value from local storage
   removeItem(key: string): void {
     localStorage.removeItem(key);
   }
 
-  // Clear all items from local storage
+  setItem(key: string, value: string): void {
+    localStorage.setItem(key, value);
+  }
+
+  getItem(key: string): string | null {
+    return localStorage.getItem(key);
+  }
+
   clear(): void {
     localStorage.clear();
   }
